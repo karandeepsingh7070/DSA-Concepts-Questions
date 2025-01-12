24a09c5f6c1bbb92610a3e5f0cda57643b70d71c
Karandeepsingh7070 <soundportrait2530@gmail.com>
2025-01-12 20:29:33 +0530

    1 question

--- Streak/Karan/LinkedList.js
+++ Streak/Karan/LinkedList.js
@@ -298,3 +298,19 @@ class Solution {
         return zeroHead.next
     }
 }
+
+// #11 Intersection of Two Linked Lists leetcode 160
+
+var getIntersectionNode = function(headA, headB) {
+    if (headA == null || headB == null) return null
+    let t1 = headA
+    let t2 = headB
+    while(t1 != t2 && t1 != null && t2 != null) {
+        t1 = t1.next
+        t2 = t2.next
+        if(t1 == t2) return t1
+        if(t1 === null) t1 = headB
+        if(t2 === null) t2 = headA
+    }
+    return t1 
+};
