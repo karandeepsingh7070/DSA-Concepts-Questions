25822cd0707033810cb4a760bdece70d48fd4253
yogesh yadav <yogesh7132@gmail.com>
2025-01-13 22:57:42 +0530

    3 Arr Question

--- Streak/Yogesh/Array.js
+++ Streak/Yogesh/Array.js
@@ -1,9 +1,56 @@
 // #1 Largest element
-//Brute
+// Brute: Sort >> arr[n-1] | O(NlogN)
+// Optimal | O(N)
 let largest = (arr) => {
-    let largest = arr?.[0]
-    arr?.forEach(ele => {
+    let largest = arr[0]
+    arr.forEach(ele => {
         if(largest < ele) largest = ele
     })
     return largest
 }
+
+// #2 Second Largest without sorting
+// Brute: Sort >> loop from arr[n-2] till arr[i] != largest | O(NlogN + N) 
+// Better: Loop for largest >> Loop for Secondlargest till arr[i] > secondLargest && arr[i] != largest | O(2N)
+// Optimal | O(N)
+function getSecondLargest(arr) {
+    let largest = arr[0]
+    let slargest = -1
+    for(let i=1; i<=arr.length-1; i++){
+        if(arr[i] > largest){
+            slargest = largest
+            largest = arr[i]
+        } else if (arr[i] < largest && arr[i] > slargest){
+            slargest = arr[i]  
+        }
+    }
+    return slargest
+}
+
+// #3 Check if the array is sorted | O(N)
+function isSorted(n, a) {
+    let isSorted = 1
+    for (let i = 0; i < n - 1; i++){
+        if (a[i] > a[i + 1]) {
+            isSorted = 0 
+        }
+    }
+    return isSorted
+}
+
+// #4 Remove duplicates from Sorted array
+var removeDuplicates = function(arr) {
+    let i = 0
+	for (let j=1; j<=arr.length; j++){
+		if(arr[i] != arr[j]) {
+            arr[i+1] = arr[j]
+            i++
+        }
+	}
+    return i+1
+};
+
+// #5 Left Rotate an array by one place
+
+
+// #6 Left rotate an array by D places
