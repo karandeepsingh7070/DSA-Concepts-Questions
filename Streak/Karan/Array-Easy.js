6d86637f153fb2ab23df63253b0121694a4271a2
Karandeepsingh7070 <soundportrait2530@gmail.com>
2025-01-18 00:22:25 +0530

    2 array questions

--- Streak/Karan/Array-Easy.js
+++ Streak/Karan/Array-Easy.js
@@ -35,3 +35,49 @@ var removeDuplicates = function(nums) {
     })
     return sSet.size
 };
+
+// #4 Rotate By K Places
+// BRUTE
+
+var rotate = function(nums, k) {
+    k = k % nums.length
+    if(k < 0) k = k + nums.length
+    let tempEle = []
+    for(let i = 0; i <= k; i++) {
+        tempEle[i] = nums[i]
+    }
+    for(let i = k + 1; i < nums.length; i++) {
+        nums[i - k] = nums[i] // i = 2, d = 2 i.e it'll start from 0 index
+    }
+    for(let i = nums.length - k; i < nums.length;i++) {
+        nums[i] = tempEle[i - (nums.length  - k)]
+    }
+}
+
+// OPTIMAL
+var rotate = function(nums, k) {
+    k = k % nums.length
+    if(k < 0) k = k + nums.length
+    let l = nums.length
+    reverseArr(nums,0,l - k - 1)
+    reverseArr(nums,l - k,l - 1)
+    reverseArr(nums,0,l - 1)
+}
+
+// #5 Move Zeroes
+var moveZeroes = function(nums) {
+    let i = -1 // should be on zero el
+    for(let k = 0; k < nums.length; k++) {
+        if(nums[k] === 0) {
+            i = k
+            break;
+        }
+    }
+    for(let j = i + 1; j < nums.length; j++) { // j should be on non zero el
+        if(nums[j] != 0 && nums[i] == 0) {
+            //swap
+            [nums[j],nums[i]] = [nums[i],nums[j]]
+            i++
+        }
+    }
+}
