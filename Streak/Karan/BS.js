7f86875aa3a3f0b5d89314355a11cadfb0312a5e
Karandeepsingh7070 <soundportrait2530@gmail.com>
2025-01-17 21:45:12 +0530

    1 BS question

--- Streak/Karan/BS.js
+++ Streak/Karan/BS.js
@@ -88,3 +88,61 @@ class Solution {
         return -1
     }
 }
+
+// #3 Koko Eating Bananas
+// BRUTE
+var findLargest = function(arr) {
+    let l = arr[0]
+    for(let i = 1; i < arr.length; i++) {
+        l = Math.max(arr[i],l)
+    }
+    return l
+}
+var minEatingSpeed = function(piles, h) {
+    let largestPile = findLargest(piles)
+    let ans = 1
+    for(let i = 1; i <= largestPile; i++) { // i = number of banana's eating
+        let reqTime = 0
+        for(let j = 0; j < piles.length; j++) {
+            reqTime += Math.ceil(piles[j]/i) // bananas/hr's
+        }
+    if(reqTime <= h) {
+        ans = i
+        break
+    }
+    }
+    return ans
+};
+
+// OPTIMAL
+var findLargest = function(arr) {
+    let l = arr[0]
+    for(let i = 1; i < arr.length; i++) {
+        l = Math.max(arr[i],l)
+    }
+    return l
+}
+var calRequiredTime = function(piles,perEatingBanana) {
+    let requiredTime = 0
+    for(let i = 0; i < piles.length; i++) {
+        requiredTime += Math.ceil(piles[i]/perEatingBanana)
+    }
+    return requiredTime
+}
+var minEatingSpeed = function(piles, h) {
+    let largestPile = findLargest(piles)
+    let low = 1
+    let high = largestPile
+    let ans = largestPile
+    while(low <= high) {
+        let mid = Math.floor((low+high)/2)
+        let requiredTime = calRequiredTime(piles,mid)
+        if(requiredTime <= h) {
+            ans = mid
+            high = mid - 1
+        }else {
+            low = mid + 1
+        }
+    }
+    return ans
+};
