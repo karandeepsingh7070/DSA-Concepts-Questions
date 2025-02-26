42cec65768921ab4e7d1e1fd033d9edd1641f4b3
Karandeepsingh7070 <soundportrait2530@gmail.com>
2025-02-26 11:01:50 +0530

    2 Questions

--- Streak/Karan/Graphs.js
+++ Streak/Karan/Graphs.js
@@ -146,3 +146,70 @@ var orangesRotting = function(grid) {
     if(rT != tFresh) return -1
     return tMax
 };
+
+// flood fill
+
+var dfs = function(image, sr, sc, color,ans,initialColor) {
+    ans[sr][sc] = color // instead of 1 will check for color as a indicator of not visited
+    // we can make a adj list and call the dfs on nbr as we do or try th four direction method
+     let n = image.length
+    let m = image[0].length
+    let dRow = [-1,0,1,0]
+    let dCol = [0,1,0,-1]
+    for(let i = 0; i < 4; i++) {
+        let nRow = sr + dRow[i]
+        let nCol = sc + dCol[i]
+        if(nRow >= 0 && nCol >= 0 && nRow < n && nCol < m && ans[nRow][nCol] != color && image[nRow][nCol] == initialColor) {
+            dfs(image, nRow, nCol, color,ans,initialColor)
+        }
+    }
+    
+}
+var floodFill = function(image, sr, sc, color) {
+    let n = image.length
+    let m = image[0].length
+    let ans = image.map(row => [...row]); //act like isVisited
+    let initialColor = image[sr][sc]
+    dfs(image, sr, sc, color,ans,initialColor)
+    return ans
+};
+
+// Detect cycle using BFS
+  class Solution {
+      // Function to detect cycle in an undirected graph.
+      detectCycle(node,isVisited,adj) {
+          isVisited[node] = true
+          let q = new CustomQueue()
+          q.enqueue({node,parent: -1})
+          
+          while(!q.isEmpty()) {
+              let frontNode = q.front()
+              let srcNode = frontNode.node
+              let parent = frontNode.parent
+              q.dequeue()
+              
+              for(const nbrNode of adj[srcNode]) {
+                  if(!isVisited[nbrNode]) {
+                      isVisited[nbrNode] = true
+                      q.enqueue({node : nbrNode, parent : srcNode})
+                  }else {
+                      if(nbrNode != parent) {
+                          return true
+                      }
+                  }
+              }
+          }
+          return false
+      }
+      
+      isCycle(adj) {
+          let isVisited = new Array(adj.length).fill(false)
+          for(let i = 0; i< isVisited.length; i++) {
+              if(!isVisited[i]) {
+                  if (this.detectCycle(i,isVisited,adj)) return true
+              }
+          }
+          
+          return false
+      }
+  }
