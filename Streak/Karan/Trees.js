7bc5c5774d51e13189eccd7e85de6db9f98264de
Karandeepsingh7070 <soundportrait2530@gmail.com>
2025-03-22 10:24:15 +0530

    5 trees questions

--- Streak/Karan/Trees.js
+++ Streak/Karan/Trees.js
@@ -127,3 +127,90 @@ var maxDepth = function(root) {
 
     return 1 + Math.max(lh,rh)
 };
+
+// is balanced tree
+var findHight = (root) => {
+    if(root == null) return 0
+
+    let lh = findHight(root.left)
+    let rh = findHight(root.right)
+
+    if(lh == -1 || rh == -1) return -1
+    if(Math.abs(lh - rh) > 1) return -1
+
+    return 1 + Math.max(lh,rh)
+}
+var isBalanced = function(root) {
+    let height = findHight(root)
+    if (height == -1) return false
+    return true
+};
+
+// Diameter of a binary tree
+var findmaxHeight = function(root,maxi) {
+    if(root == null) return 0
+
+    let lh = findmaxHeight(root.left,maxi)
+    let rh = findmaxHeight(root.right,maxi)
+
+    maxi.val = Math.max(maxi.val,lh+rh)
+    return 1 + Math.max(lh,rh)
+}
+var diameterOfBinaryTree = function(root) {
+    let maxi = {val : 0}
+    findmaxHeight(root,maxi)
+    return maxi.val
+};
+// Max sum path
+var findmaxSum = function(root, maxi) {
+    if(root == null) return 0
+
+    let leftSum = Math.max(0,findmaxSum(root.left,maxi))
+    let rightSum = Math.max(0,findmaxSum(root.right,maxi))
+    maxi.val = Math.max(maxi.val,leftSum + rightSum + root.val)
+    return root.val + Math.max(leftSum,rightSum)
+}
+var maxPathSum = function(root) {
+    let maxi = {val:-Infinity}
+    findmaxSum(root,maxi)
+    return maxi.val
+};
+
+// is same tree
+var isSameTree = function(p, q) {
+    if(p == null || q == null) {
+        return p == q
+    }
+    return p.val == q.val && 
+    isSameTree(p.left,q.left) && 
+    isSameTree(p.right,q.right)
+};
+
+// print zigzag traversals
+
+var zigzagLevelOrder = function(root) {
+
+    if(root == null) return []
+    let ans = []
+    let flag = 0
+    let q = new CustomQueue()
+
+    q.enqueue(root)
+
+    while(!q.isEmpty()) {
+        let size = q.size()
+        let level = []
+
+        for(let i = 0; i < size; i++) {
+            let front = q.front()
+            q.dequeue()
+            if(front.left != null) q.enqueue(front.left)
+            if(front.right != null) q.enqueue(front.right)
+            level.push(front.val)
+        }
+        if(!flag) ans.push(level)
+        else ans.push([...level].reverse())
+        flag = !flag
+    }
+    return ans
+};
