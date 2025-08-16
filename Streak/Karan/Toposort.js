8d7febad2abde065b505d6c9f554e467feb70ed6
Karandeepsingh7070 <soundportrait2530@gmail.com>
2025-08-16 14:58:21 +0530

    kahn's algorithm

--- Streak/Karan/Toposort.js
+++ Streak/Karan/Toposort.js
@@ -47,3 +47,60 @@ class Solution {
       return st.getStack()
     }
 }
+
+class CustomQueue {
+    constructor() {
+        this.items = []
+    }
+    enqueue(item) {
+        return this.items.push(item)
+    }
+    dequeue() {
+        // Array.shift is O(n) instead using Array.splice is preferable
+        return this.items.shift()
+    }
+    isEmpty() {
+        return this.items.length === 0
+    }
+}
+
+// Topological Sort using Kahn's Algorithm
+class Solution {
+    convertToAdjList(n, list) {
+       let adjList = new Array(n).fill().map(() => []);
+       for (const [u, v] of list) {
+           adjList[u].push(v); 
+       }
+       return adjList
+   }
+    
+    topoSort(V, edges) {
+        // create all inDegree nodes
+        let inDegree = new Array(V).fill(0)
+        let adjList = this.convertToAdjList(V,edges)
+        for(let i = 0; i < V; i++) {
+            for(const elm of adjList[i]) {
+                inDegree[elm]++
+            }
+        }
+        let q = new CustomQueue()
+        // add all zero indegree elements to queue
+        for(let i = 0;  i < V; i++) {
+            if(inDegree[i] === 0) q.enqueue(i)
+        }
+        let top = []
+        while(!q.isEmpty()) {
+            let front = q.dequeue()
+            top.push(front)
+            
+            for(const elm of adjList[front]) {
+                inDegree[elm]--
+                if(inDegree[elm] == 0) {
+                    q.enqueue(elm)
+                }
+            }
+        }
+        return top
+        
+    }
+}
